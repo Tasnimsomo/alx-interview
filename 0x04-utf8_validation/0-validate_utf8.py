3d04aedc77c224ba0f2aed8a712e8390263a583b
root <root@TasnimSomo>
2024-07-25 21:31:28 +0400

    0x04. UTF-8 Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,34 +2,28 @@
 
 """0. UTF-8 Validation"""
 
-
 def validUTF8(data):
-    """
-    Write a method that determines if a given data
-    set represents a valid UTF-8 encoding
-    """
+    """ utf 8 validation"""
     counter = 0
     for byte in data:
         byte_bin = format(byte, '08b')  # Convert byte to binary string
         if counter == 0:
             if byte_bin[0] == '0':
-                if counter != 0:
-                    return False
-                continue
+                continue  # Single-byte character
             elif byte_bin[:3] == '110':
-                counter = 1
+                counter = 1  # Start of 2-byte character
             elif byte_bin[:4] == '1110':
-                counter = 2
+                counter = 2  # Start of 3-byte character
             elif byte_bin[:5] == '11110':
-                counter = 3
+                counter = 3  # Start of 4-byte character
             else:
-                return False
+                return False  # Invalid UTF-8 start byte
         else:
             if byte_bin[:2] != '10':
-                return False
+                return False  # Invalid continuation byte
             counter -= 1
-
+            
             if counter < 0:
-                return False
+                return False  # More continuation bytes than expected
 
-    return counter == 0
+    return counter == 0  # Valid if counter is 0 at the end
