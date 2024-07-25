15ef26fd9055e0f30aef91ebc54b621d452f407e
root <root@TasnimSomo>
2024-07-25 21:19:35 +0400

    0x04. UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def validUTF8(data):
+    counter = 0
+    for byte in data:
+        byte_bin = format(byte, '08b')  # Convert byte to binary string
+        if counter == 0:
+            if byte_bin[0] == '0':
+                continue
+            elif byte_bin[:3] == '110':
+                counter = 1
+            elif byte_bin[:4] == '1110':
+                counter = 2
+            elif byte_bin[:5] == '11110':
+                counter = 3
+            else:
+                return False
+        else:
+            if byte_bin[:2] != '10':
+                return False
+            counter -= 1
+    if counter != 0:
+        return False
+    return True
